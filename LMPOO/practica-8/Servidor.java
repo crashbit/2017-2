172a9636cd4d675b412eb48d9500d493084da781
Germán Santos Jaimes <germans@unam.mx>
2017-03-29 16:26:40 -0600

    Servidor socket

---
+++ LMPOO/practica-8/Servidor.java
@@ -0,0 +1,30 @@
+import java.net.*;
+import java.io.*;
+
+public class Servidor{
+
+	public static void main(String args[])throws IOException{
+		ServerSocket servidor;
+		Socket usuario;
+
+		InputStreamReader entrada;
+		PrintWriter salida;
+		BufferedReader flujo;
+		String cadena;
+
+
+		servidor = new ServerSocket(3000);
+		usuario = servidor.accept();
+
+		entrada = new InputStreamReader(usuario.getInputStream());
+		salida = new PrintWriter(usuario.getOutputStream(), true);
+		flujo = new BufferedReader(entrada);
+
+		salida.println("Hola usuario... :) ");
+
+
+	}
+
+
+
+}
