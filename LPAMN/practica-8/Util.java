88f6c8f5de73b2449ced2b948b364b4dd1c629c9
Germán Santos Jaimes <germans@unam.mx>
2017-03-31 10:50:45 -0600

    Streams en java

---
+++ LPAMN/practica-8/Util.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Util{
+
+	public static void main(String args[]){
+
+		Scanner teclado = new Scanner(System.in);
+		String cadena;
+		int numero;
+
+		cadena = teclado.nextLine();
+		numero = teclado.nextInt();
+		System.out.println(cadena);
+		System.out.println(numero * 3);
+
+
+	}
+}
